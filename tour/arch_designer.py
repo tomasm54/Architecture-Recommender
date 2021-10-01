ca8f81d04d24b32b985b381c8030d4daca5c76ea
gianluca <gcapozzo@alumnos.exa.unicen.edu.ar>
2021-10-01 10:53:33 -0300

    renamed methods, code comments and a way to avoid repeated architectures detections

--- tour/arch_designer.py
+++ tour/arch_designer.py
@@ -1,7 +1,6 @@
 import json
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+from typing import Optional
+
 import es_core_news_lg
 
 doclg = es_core_news_lg.load()
@@ -16,6 +15,7 @@ with open(requirements_path) as requirements_file:
     requirements_data = json.load(requirements_file)
 
 user_requirements = []
+recognized_architectures = []
 
 
 def compare_str_sintax(str1: str, str2: str) -> bool:
@@ -51,21 +51,24 @@ def load_requirements(values: dict) -> list:
     requirements.extend(values["requirements"]["own"])  # MINIMUM ONE AT THE MOMENT
     return requirements
 
-class ArchitectureDesigner():
 
-    def run(message : str) -> str:
-        user_requirements.append(message)  # add last requirement to requirements list
-        # for each "case" (requirements set - architecture) check if the reqs have same sintax
+def find_architecture(last_requirement: str) -> Optional[str]:
+    """
+    Returns an architecture if are recognized with last requirement @message
+    """
+    user_requirements.append(last_requirement)  # add last requirement to requirements list
+    # for each "case" (requirements set - architecture) check if all reqs have same sintax to match an architecture
+    same_sintax_reqs = 0
+    for arch_id, arch in architectures_data.items():  # for each architecture
+        arch_reqs = load_requirements(arch)
+        for requirement in user_requirements:  # for each user requirement
+            for arch_req in arch_reqs:  # for each requirement of the architecture
+                # check if any user requirement match with arch requirement
+                if compare_str_sintax(requirement, arch_req):
+                    same_sintax_reqs += 1
+        if same_sintax_reqs == len(arch_reqs) and (arch_id not in recognized_architectures):
+            print("arch:" + arch["architecture"]["type"])
+            recognized_architectures.append(arch_id)
         same_sintax_reqs = 0
-        for arch in architectures_data.values():
-            arch_reqs = load_requirements(arch)
-            for requirement in user_requirements:
-                for arch_req in arch_reqs:
-                    if compare_str_sintax(requirement, arch_req):
-                        same_sintax_reqs += 1
-            if same_sintax_reqs == len(arch_reqs):
-                return arch["architecture"]["type"]
-            same_sintax_reqs = 0
 
-        return None
-
+    return None
